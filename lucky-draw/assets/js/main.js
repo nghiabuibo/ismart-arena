90d0b9e04954ab1ed5b217ee6ed0ea291eaac9e1
Bùi Như Nghĩa <nghia.bui@blueocean.net.vn>
2024-01-18 03:09:50 +0700

    add lucky draw

---
+++ lucky-draw/assets/js/main.js
@@ -0,0 +1,194 @@
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const bgm = document.getElementById("bgm");
+
+function openDialog() {
+	var dialog = document.getElementById("dialog");
+	dialog.style.display = "flex";
+}
+
+function closeDialog() {
+	var dialog = document.getElementById("dialog");
+	dialog.style.display = "none";
+}
+
+function initRandom() {
+	$.getJSON("result.json?" + Date.now(), function (result) {
+		if (!result || !Object.keys(result).length) return;
+
+		for (let index in result) {
+			const winnerIDs = result[index]
+			winnerIDs.forEach(id => {
+				setWinner(index, id)
+			})
+		}
+
+		$('#list_control').addClass('active')
+	});
+}
+initRandom();
+
+function saveResult() {
+	const winner_order = []
+	$('.prize-block').each(function () {
+		const winnerIDsStr = $(this).data('winner-id')
+		const winnerIDs = winnerIDsStr ? winnerIDsStr.split(',') : []
+		winner_order.push(winnerIDs)
+	})
+	$.ajax({
+		type: "POST",
+		dataType: 'json',
+		url: 'save.php',
+		data: { winner_order: winner_order },
+	});
+}
+
+function setWinner(index, id) {
+	if (!id) return;
+
+	// set data attr
+	const prizeBlock = $('.prize-block').eq(index)
+	const winnerIDsStr = prizeBlock.data('winner-id')
+	const winnerIDs = winnerIDsStr ? winnerIDsStr.split(',') : []
+	winnerIDs.push(id)
+	prizeBlock.data('winner-id', winnerIDs.join(','))
+
+	// get info from blocks
+	const block = $(`.block[data-id="${id}"]`)
+	const name = block.data('name')
+	const phone = block.data('phone')
+	const img = prizeBlock.data('img')
+
+	// set prize blocks info
+	prizeBlock.find('.prize-id').text(winnerIDs.join(' - '))
+	// prizeBlock.find('.prize-name').text(name)
+	// prizeBlock.find('.prize-phone').text(phone)
+	// prizeBlock.find('.prize-img').attr('src', img)
+
+	// set blocks final class
+	$(`.block[data-id="${id}"]`).addClass('final');
+
+	// set dialog info
+	const dialog = $('#dialog')
+	dialog.find('.dialog-id').text(id)
+	dialog.find('.dialog-name').text(name)
+	dialog.find('.dialog-phone').text(phone)
+	dialog.find('.dialog-img').attr('src', img)
+}
+
+function random(index, steps, interval, region) {
+	const ids = [];
+	const idsRegion = []
+	let blocks = $('.block').not('.final')
+	blocks.each(function () {
+		const id = $(this).data('id')
+		ids.push(id)
+	})
+	if (region) blocks = blocks.filter(`[data-region="${region}"]`);
+	blocks.each(function () {
+		const id = $(this).data('id')
+		idsRegion.push(id)
+	})
+
+	let currentStep = 0
+	let highlight = setInterval(function () {
+		let randomIndex = null
+		while (randomIndex === null || $(`.block[data-id="${ids[randomIndex]}"]`).hasClass('final')) {
+			randomIndex = Math.floor((Math.random() * ids.length));
+		}
+		const randomID = ids[randomIndex]
+		const randomBlock = $(`.block[data-id="${randomID}"]`)
+
+		randomBlock.addClass('highlight');
+		let timeout = setTimeout(function () {
+			randomBlock.removeClass('highlight');
+		}, interval);
+
+		currentStep++
+		if (currentStep > steps) {
+			clearInterval(highlight)
+			let randomIndexRegion = null
+			while (randomIndexRegion === null || $(`.block[data-id="${idsRegion[randomIndexRegion]}"]`).hasClass('final')) {
+				randomIndexRegion = Math.floor((Math.random() * idsRegion.length));
+			}
+			const randomIDRegion = idsRegion[randomIndexRegion]
+
+			setTimeout(() => {
+				setWinner(index, randomIDRegion)
+				saveResult()
+			}, interval)
+
+			// show congratulations
+			if (randomIDRegion) {
+				setTimeout(openDialog, 3 * interval)
+				// openDialog()
+			}
+		}
+	}, interval);
+}
+
+$('.start-random').on('click', function () {
+	const currentPrize = $(this).closest('.prize-block')
+
+	if (!currentPrize.length) return;
+
+	$('.close').addClass('d-none');
+	bgm.play();
+
+	const index = $('.prize-block').index(currentPrize)
+	const region = currentPrize.data('region')
+	const steps = 20
+	const interval = 300
+	const duration = (steps + 3) * interval
+	random(index, steps, interval, region)
+
+	setTimeout(() => {
+		$('.close').removeClass('d-none');
+		bgm.pause();
+		bgm.currentTime = 0
+	}, duration)
+});
+
+$('.prize-img').on('click', function () {
+	const currentPrize = $(this).closest('.prize-block')
+	const winnerIDsStr = currentPrize.data('winner-id')
+	const winnerIDs = winnerIDsStr ? winnerIDsStr.split(',') : []
+
+	const img = currentPrize.data('img')
+	$('#prize_list_img').attr('src', img)
+
+	$('#prize_list_table tbody').html('')
+
+	winnerIDs.forEach((id, index) => {
+		// get info from blocks
+		const block = $(`.block[data-id="${id}"]`)
+		const name = block.data('name')
+		const phone = block.data('phone')
+
+		$('#prize_list_table tbody').append(`
+			<tr>
+				<td>${index + 1}</td>
+				<td>${id}</td>
+				<td>${name}</td>
+				<td>${phone}</td>
+			</tr>
+		`)
+	})
+})
+
+$('#clear_list').on('click', function () {
+	var passcode = prompt("Please enter passcode");
+	if (passcode !== 'abc@123') return;
+
+	$.ajax({
+		type: "POST",
+		url: 'save.php',
+		data: { clear: '1' },
+		success: function (response) {
+			location.reload();
+		}
+	});
+});
---
+++ lucky-draw/index.php
@@ -0,0 +1,220 @@
+<?php if (file_exists(__DIR__ . '/config.php')) include_once(__DIR__ . '/config.php'); ?>
+<?php $cache = '241301'; ?>
+<!DOCTYPE html>
+<html lang="vi-VN">
+
+<head>
+	<meta charset="utf-8" />
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<link href="http://gmpg.org/xfn/11" rel="profile" />
+	<title>
+		Lucky Draw - iSmart Online
+	</title>
+	<meta content="vi_VN" property="og:locale" />
+	<meta content="article" property="og:type" />
+	<meta content="Lucky Draw - iSmart Online" property="og:title" />
+	<meta content="Lucky Draw - iSmart Online" property="og:description" />
+	<meta content="iSmart Online" property="og:site_name" />
+	<meta content="assets/imgs/bg.jpg" property="og:image" />
+	<meta content="Lucky Draw - iSmart Online" name="twitter:card" />
+	<meta content="Lucky Draw - iSmart Online" name="twitter:description" />
+	<meta content="Lucky Draw - iSmart Online" name="twitter:title" />
+	<meta content="assets/imgs/bg.jpg" name="twitter:image" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="favicon.ico" type="image/ico" />
+
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css?v=<?php echo $cache; ?>" />
+</head>
+
+<body>
+	<div class="container wrapper">
+		<div class="row g-md-5">
+			<div class="col text-end">
+				<img class="logo" src="assets/imgs/ism-logo.png" />
+			</div>
+			<div class="col">
+				<img class="logo" src="assets/imgs/event-logo.png" />
+			</div>
+		</div>
+		<div class="row">
+			<h1 class="text-center display-1 fw-bold title my-3">Lucky Draw</h1>
+		</div>
+		<div class="row mt-3">
+			<div class="col actions text-center">
+				<button class="btn btn-primary" id="show_list" data-bs-target="#winner_list" data-bs-toggle="modal">Xem danh sách</button>
+			</div>
+		</div>
+		<div class="row">
+			<?php
+			$next = 0;
+			for ($i = 0; $i < 3; $i++) {
+				$next = $i + 1;
+			?>
+				<div class="col-md-4">
+					<div class="prize-block" data-img="assets/imgs/prize-<?php echo $i + 1; ?>.png?v=<?php echo $cache; ?>">
+						<img class="img-fluid prize-img" src="assets/imgs/prize-<?php echo $i + 1; ?>.png?v=<?php echo $cache; ?>" role="button" data-bs-target="#prize_list" data-bs-toggle="modal" />
+						<div class="prize-id text-center fw-bold mt-3 lead"></div>
+						<div class="prize-name text-center fw-bold"></div>
+						<div class="prize-phone text-center fw-bold"></div>
+						<div class="text-center mt-3">
+							<button class="btn btn-success start-random" data-bs-target="#winner_list" data-bs-toggle="modal">Quay số</button>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+		<div class="row">
+			<?php for ($i = 0; $i < 2; $i++) { ?>
+				<div class="col-md-4 <?php if ($i === 0) echo 'offset-md-2'; ?>">
+					<div class="prize-block" data-img="assets/imgs/prize-<?php echo $i + $next + 1; ?>.png?v=<?php echo $cache; ?>">
+						<img class="img-fluid prize-img" src="assets/imgs/prize-<?php echo $i + $next + 1; ?>.png?v=<?php echo $cache; ?>" role="button" data-bs-target="#prize_list" data-bs-toggle="modal" />
+						<div class="prize-id text-center fw-bold mt-3 lead"></div>
+						<div class="prize-name text-center fw-bold"></div>
+						<div class="prize-phone text-center fw-bold"></div>
+						<div class="text-center mt-3">
+							<button class="btn btn-success start-random" data-bs-target="#winner_list" data-bs-toggle="modal">Quay số</button>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+
+	<audio src="assets/bgm.mp3" type="audio/mpeg" id="bgm">
+		Your browser does not support the audio element.
+	</audio>
+
+	<div id="list_control">
+		<button type="button" class="btn btn-danger" id="clear_list">
+			Xóa danh sách
+		</button>
+	</div>
+
+	<!-- Modal -->
+	<div class="modal fade" id="winner_list" tabindex="-1" aria-labelledby="winner_list_label" aria-hidden="true">
+		<div class="modal-dialog modal-fullscreen">
+			<div class="modal-content">
+				<div class="modal-body">
+					<!--<button class="btn btn-light" id="toggle_size">Small</button>-->
+					<div class="prize-blocks">
+						<?php
+						$api_url = defined('API_URL') ? API_URL : null;
+						$token = defined('RESULT_TOKEN') ? RESULT_TOKEN : null;
+						if ($api_url && $token) {
+							$params = [
+								'filters[results][answers][$not]' => 'null'
+							];
+							$endpoint = $api_url . '/users?' . http_build_query($params);
+
+							// Initialize cURL session
+							$ch = curl_init();
+
+							// Set cURL options
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, [
+								'Authorization: Bearer ' . $token,
+							]);
+							curl_setopt($ch, CURLOPT_URL, $endpoint);
+
+							// Execute cURL session and get the result
+							$response = curl_exec($ch);
+
+							// Check for errors
+							if (curl_errno($ch)) {
+								echo 'Curl error: ' . curl_error($ch);
+							}
+
+							// Close cURL session
+							curl_close($ch);
+
+							// Output the response
+							$data_arr = json_decode($response, true);
+
+							if ($data_arr) {
+								foreach ($data_arr as $key => $data) {
+									$id = $data['id'] ?? '';
+									if (!$id) continue;
+
+									$name = $data['name'] ?? '';
+									$phone = $data['phone'] ?? '';
+									if ($phone) $phone = '******' . substr($phone, -4);
+
+									$region = $data['grade'] ?? '';
+
+									$tooltip = [];
+									if ($name) $tooltip[] = $name;
+									if ($phone) $tooltip[] = $phone;
+									$tooltip_html = implode('<br/>', $tooltip);
+
+									echo "<span type='button' class='block' data-id='$id' data-region='$region' data-bs-toggle='tooltip' data-bs-html='true' data-name='$name' data-phone='$phone' title='$tooltip_html'>$id</span>";
+								}
+							} else {
+								echo '<span class="text-white">No data set!</span>';
+							}
+						} else {
+							echo '<span class="text-white">Config not set!</span>';
+						}
+						?>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn close btn-secondary" data-bs-dismiss="modal">Quay về</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Prize Modal -->
+	<div class="modal fade" id="prize_list" tabindex="-1" aria-labelledby="winner_list_label" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered modal-lg">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="row align-items-center">
+						<div class="col-lg-4">
+							<img class="img-fluid prize-img" id="prize_list_img" src="" />
+						</div>
+						<div class="col-lg-8">
+							<table class="table table-bordered" id="prize_list_table">
+								<thead>
+									<tr>
+										<th>STT</th>
+										<th>ID</th>
+										<th>Tên</th>
+										<th>Số điện thoại</th>
+									</tr>
+								</thead>
+								<tbody>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn close btn-secondary" data-bs-dismiss="modal">Quay về</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Dialog -->
+	<div id="dialog" class="dialog">
+		<div class="dialog-content">
+			<img class="dialog-img img-fluid" src="assets/imgs/400.svg" />
+			<div class="p-3 text-center fw-bold">
+				<p class="dialog-id lead fw-bold"></p>
+				<p class="dialog-name"></p>
+				<p class="dialog-phone"></p>
+				<button class="btn btn-secondary" onclick="closeDialog()">Đóng</button>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+	<script type="text/javascript" src="assets/js/main.js?v=<?php echo $cache; ?>"></script>
+</body>
+
+</html>
